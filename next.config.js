70b576a4c11dc28a6e1ba669bc1a8a0aa8b7bf1f
Gilberto Suarez <suarezgilberto@hotmail.com>
2021-07-10 11:43:25 -0400

    chore: allow svg files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.resolve.alias["@app"] = path.resolve(__dirname, "../src");
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
